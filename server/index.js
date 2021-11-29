9b0edad919a19d3efb80e581f57e41fc15f9a0f9
Mitya Kim <mitya_kim@icloud.com>
2021-11-29 14:01:05 -0800

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+app.get("/api/users", (req, res) => {
+    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+    res.status(200).send(friends);
+    })
+
+app.get("/weather/:temperature", (req, res) => {
+    const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
+    res.status(200).send(phrase);
+    });
+
+app.get("/weather/:temperature", (req, res) => {
+    const { temperature } = req.params;
+    const phrase = `<h3>It was ${temperature} today</h3>`;
+    res.status(200).send(phrase);
+    });
+
+
+app.listen(4000, ()=> console.log("Server port 4000"))
